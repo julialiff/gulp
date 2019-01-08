20a4cbaab27406f459dcad323d9a4c3712c2cb0e
wkimmy <wkimmy@u9474ccbbe0b050.ant.amazon.com>
2019-01-08 18:08:16 -0200

    creating tasks on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,48 @@
+var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var useref = require('gulp-useref');
+var imagemin  =require('gulp-imagemin');
 
-gulp.task('sass', function(){
-  return gulp.src('source-files')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('destination'))
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    })
+})
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
 });
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+});
+
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(imagemin({
+        interlaced: true
+      }))
+    .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('build',gulp.parallel('sass', 'useref', 'images', 'fonts'), function (){
+    console.log('Building files....');
+})
