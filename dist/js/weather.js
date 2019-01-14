5a0830029804a5eadcdd8ad5bfc5958c2dd452ce
wkimmy <wkimmy@u9474ccbbe0b050.ant.amazon.com>
2019-01-14 17:55:52 -0200

    changed interface

--- dist/js/weather.js
+++ dist/js/weather.js
@@ -1,30 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express()
-
-const apiKey = '1eea1ba74f3e09bad7c8758072e0c0dc';
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/weather', function (req, res) {
-  console.log(req.body.city);
-  console.log(apiKey);
-  var city = req.body.city;
-  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    } else {
-      const weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
-      } else {
-        const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
-      }
-    }
-  });
-})
+function call(city){
+  alert("db");
+  $.post("http://localhost:3009",
+    {
+      name: "São Paulo"
+    })
+}
