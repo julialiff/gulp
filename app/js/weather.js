b5a730eccc4dd1887e8f24e974c0c581a5496644
wkimmy <wkimmy@u9474ccbbe0b050.ant.amazon.com>
2019-01-17 11:27:38 -0200

    minor changes

--- app/js/weather.js
+++ app/js/weather.js
@@ -1,7 +1,22 @@
-function call(city){
-  alert("db");
-  $.post("http://localhost:3009",
-    {
-      name: "São Paulo"
-    })
+
+function CheckWeather(){
+    var city = document.getElementById('city').value;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${''}`
+    var request = new XMLHttpRequest();
+    
+    request.open('GET', url, true);
+    request.onload = function () {
+        console.log("entrei");
+        var weather = JSON.parse(this.response);
+        console.log(weather);
+        if (request.status >= 200 && request.status < 400) {
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            alert(weatherText);
+            document.getElementById('resp').value = weatherText;
+        } else {
+            document.getElementById('resp').value = ('Oops something went wrong :(');
+        }
+    }
+    request.send();
 }
+
